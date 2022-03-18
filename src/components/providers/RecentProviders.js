705bc4cfe6e8e8cecdb99ae670c48508ec0db07b
brianminges <btminges@gmail.com>
2022-03-17 21:44:03 -0500

    recently added bar set

--- src/components/providers/RecentProviders.js
+++ src/components/providers/RecentProviders.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getAllProviders } from "./../../modules/ProviderManager"
 import "./RecentProviders.css"
 
 export const RecentProviders = ({provider}) =>  {
+    const sessionUser = JSON.parse(window.sessionStorage.getItem("homegroan_user"))
+    const sessionUserId = sessionUser.id;
+    
+    const [providers, setProviders] = useState([]);
+
+    const getProviders = () => {
+        return getAllProviders(sessionUserId).then(dataFromAPI => {
+            setProviders(dataFromAPI)
+        });
+    };
+
+    useEffect(() => {
+        getProviders()
+    }, [])
 
     return (
         <>
         <div className="recent__cards">
             <div className="recent__card">
-                <p><strong>{provider.name}</strong></p>
+                <Link to={`/ServiceProviders/${provider.id}`}><p><strong>{provider.name}</strong></p></Link>
                 <p>Added {provider.timestamp}</p>
                 <div className="spacer"></div>
             </div>
@@ -15,3 +31,6 @@ export const RecentProviders = ({provider}) =>  {
         </>
     )
 }
+
+
+
